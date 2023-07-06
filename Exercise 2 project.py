991bc4293bc974d685b40cbe87ebb780c0a7b1c1
fedgy1993 <138087649+fedgy1993@users.noreply.github.com>
2023-07-06 10:41:12 -0400

    Add files via upload

---
+++ Exercise 2 project.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Execute it as a markdown cell
+# # H1: Data Science Tools and Ecosystem
+# ## H2: Data management tools like My SQL, MongoDB, Apache CouchDB and Apache Cassandra
+# ### H3: Data integration tools Apache Airflow, Kubeflow, Apache Kafka, Apache Nifi
+# ##### H4: Data Visualization Apache Superset, Apache PredictionIO, R, pixie, Dust
+# ###### H5: Model Deployment Seldon, Kubernets, Redhat, Mleap, Tensorflow service
+# ###### H6: Code asset management are Git, Github, Gitlab, 
+# ###### H7: data asset management Apache Atlas, ODPi Egeria, Kylo
+
+# In[ ]:
+
+
+
+
